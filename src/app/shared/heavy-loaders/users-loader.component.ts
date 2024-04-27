1924cb20dcf426c8a4cb2e80a28d43ef5eb3c5e7
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-04-27 13:21:38 -0500

    Create components

---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-loader',
+  standalone: true,
+  imports: [],
+  template: ` <h1>Hola mundo</h1> `,
+})
+export class UsersLoaderComponent {}
