a72a931be123c0458836b7e58f0901b9443007f8
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-04-27 14:03:17 -0500

    Sidebar desing

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sidemenu.component.html',
-  styleUrl: './sidemenu.component.css'
+  styleUrl: './sidemenu.component.css',
 })
 export class SidemenuComponent {
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'))
+    .filter((route) => !route.path?.includes('**'));
 
+  constructor() {}
 }
